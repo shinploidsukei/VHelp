ff15a2bb8f21476c42f5270fc649225f1b98007e
shinploidsukeii_ <ployploy1900@hotmail.com>
2021-08-30 19:20:44 +0700

    DocNoti Ver1

--- lib/DocNoti.dart
+++ lib/DocNoti.dart
@@ -1,40 +1,152 @@
-import 'package:provider/provider.dart';
-import 'package:vhelp_test/date_picker_widget.dart';
-import 'package:vhelp_test/provider/event_provider.dart';
-import 'package:vhelp_test/time_picker_widget.dart';
 import 'package:flutter/material.dart';
-import 'content.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/data/latest.dart' as tz;
+import 'package:table_calendar/table_calendar.dart';
 
-FlutterLocalNotificationsPlugin notificationsPlugin =
-FlutterLocalNotificationsPlugin();
-
-class DocNoti extends StatelessWidget {
-  static final String title = 'Date (Range) & Time';
-
+class DocNoti extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(create:(context)=> EventProvider(), 
-  
-  child:MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: title,
-    theme: ThemeData(
-      primaryColor: Colors.blueGrey,
-    ),
-    home: DocNotiPage(),
-   ) );
+  _DocNotiPage createState() => _DocNotiPage();
 }
 
-class DocNotiPage extends StatelessWidget {
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: Text('Doctor Notification'),
-      centerTitle: true,
-    ),
-    body: Scaffold(
+
+class _DocNotiPage extends State<DocNoti> {
+DateTime selectedDate = DateTime.now(); // TO tracking date
+
+  int currentDateSelectedIndex = 0; //For Horizontal Date
+  ScrollController scrollController =
+      ScrollController(); //To Track Scroll of ListView
+
+  List<String> listOfMonths = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+
+  List<String> listOfDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        automaticallyImplyLeading: false,
+        title: Text('My Calendar'),
       ),
-    
-  );
+      body: Column(
+        children: [
+          //To Show Current Date
+          Container(
+              height: 30,
+              margin: EdgeInsets.only(left: 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                selectedDate.day.toString() +
+                    '-' +
+                    listOfMonths[selectedDate.month - 1] +
+                    ', ' +
+                    selectedDate.year.toString(),
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.indigo[700]),
+              )),
+          SizedBox(height: 10),
+          //To show Calendar Widget
+          Container(
+              height: 80,
+              child: Container(
+                  child: ListView.separated(
+                separatorBuilder: (BuildContext context, int index) {
+                  return SizedBox(width: 10);
+                },
+                itemCount: 365,
+                controller: scrollController,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return InkWell(
+                    onTap: () {
+                      setState(() {
+                        currentDateSelectedIndex = index;
+                        selectedDate =
+                            DateTime.now().add(Duration(days: index));
+                      });
+                    },
+                    child: Container(
+                      height: 80,
+                      width: 60,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.shade400,
+                                offset: Offset(3, 3),
+                                blurRadius: 5)
+                          ],
+                          color: currentDateSelectedIndex == index
+                              ? Colors.black
+                              : Colors.white),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            listOfMonths[DateTime.now()
+                                        .add(Duration(days: index))
+                                        .month -
+                                    1]
+                                .toString(),
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: currentDateSelectedIndex == index
+                                    ? Colors.white
+                                    : Colors.grey),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            DateTime.now()
+                                .add(Duration(days: index))
+                                .day
+                                .toString(),
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w700,
+                                color: currentDateSelectedIndex == index
+                                    ? Colors.white
+                                    : Colors.grey),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            listOfDays[DateTime.now()
+                                        .add(Duration(days: index))
+                                        .weekday -
+                                    1]
+                                .toString(),
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: currentDateSelectedIndex == index
+                                    ? Colors.white
+                                    : Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ))),
+        ],
+      ),
+    ));
+  }
 }

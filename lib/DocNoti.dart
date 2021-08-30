17c8f5b225d89267dc6e9e9382290a00fdb35978
shinploidsukeii_ <ployploy1900@hotmail.com>
2021-08-30 21:05:00 +0700

    Update DocNoti.dart

--- lib/DocNoti.dart
+++ lib/DocNoti.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:vhelp_test/page/event_editing_page.dart';
+import 'package:vhelp_test/widget/calendar_widget.dart';
 
 class DocNoti extends StatefulWidget {
   @override
@@ -11,168 +10,20 @@ class DocNoti extends StatefulWidget {
 
 
 class _DocNotiPage extends State<DocNoti> {
+@override 
 
-  CalendarController? _controller;
-  Map<DateTime, List<dynamic>>? _events;
-  List<dynamic>? _selectedEvents;
-  TextEditingController? _eventController;
-  SharedPreferences? prefs;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = CalendarController();
-    _eventController = TextEditingController();
-    _events = {};
-    _selectedEvents = [];
-    prefsData();
-  }
-
-  prefsData() async {
-    prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _events = Map<DateTime, List<dynamic>>.from(
-          decodeMap(json.decode(prefs!.getString("events") ?? "{}")));
-    });
-  }
-
-  Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
-    Map<String, dynamic> newMap = {};
-    map.forEach((key, value) {
-      newMap[key.toString()] = map[key];
-    });
-    return newMap;
-  }
-  Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
-    Map<DateTime, dynamic> newMap = {};
-    map.forEach((key, value) {
-      newMap[DateTime.parse(key)] = map[key];
-    });
-    return newMap;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.blueGrey,
-        title: Text('Doctor Notification'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TableCalendar(
-              events: _events,
-              initialCalendarFormat: CalendarFormat.week,
-              calendarStyle: CalendarStyle(
-                  canEventMarkersOverflow: true,
-                  todayColor: Colors.orange,
-                  selectedColor: Theme.of(context).primaryColor,
-                  todayStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white)),
-              headerStyle: HeaderStyle(
-                centerHeaderTitle: true,
-                formatButtonDecoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                formatButtonTextStyle: TextStyle(color: Colors.white),
-                formatButtonShowsNext: false,
-              ),
-              startingDayOfWeek: StartingDayOfWeek.monday,
-              onDaySelected: (date, events,holidays) {
-                setState(() {
-                  _selectedEvents = events;
-                });
-              },
-              builders: CalendarBuilders(
-                selectedDayBuilder: (context, date, events) => Container(
-                    margin: const EdgeInsets.all(4.0),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(10.0)),
-                    child: Text(
-                      date.day.toString(),
-                      style: TextStyle(color: Colors.white),
-                    )),
-                todayDayBuilder: (context, date, events) => Container(
-                    margin: const EdgeInsets.all(4.0),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.circular(10.0)),
-                    child: Text(
-                      date.day.toString(),
-                      style: TextStyle(color: Colors.white),
-                    )),
-              ),
-              calendarController: _controller,
-            ),
-            ..._selectedEvents!.map((event) => Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: MediaQuery.of(context).size.height/20,
-                width: MediaQuery.of(context).size.width/2,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey)
-                ),
-                child: Center(
-                    child: Text(event,
-                      style: TextStyle(color: Colors.blue,
-                          fontWeight: FontWeight.bold,fontSize: 16),)
-                ),
-              ),
-            )),
-          ],
+Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Doctor Notification'),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
-        child: Icon(Icons.add),
-        onPressed: _showAddDialog,
-      ),
-    );
-  }
-
-  _showAddDialog() async {
-    await showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          backgroundColor: Colors.white70,
-          title: Text("Add Notification"),
-          content: TextField(
-            controller: _eventController,
+        body: CalendarWidget(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add, color: Colors.white),
+          backgroundColor: Colors.red,
+          onPressed: () => Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => EventEditingPage()),
           ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text("Save",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),),
-              onPressed: () {
-                if (_eventController!.text.isEmpty) return;
-                setState(() {
-                  if (_events![_controller!.selectedDay] != null) {
-                    _events![_controller!.selectedDay]!
-                        .add(_eventController!.text);
-                  } else {
-                    _events![_controller!.selectedDay] = [
-                      _eventController!.text
-                    ];
-                  }
-                  prefs!.setString("events", json.encode(encodeMap(_events!)));
-                  _eventController!.clear();
-                  Navigator.pop(context);
-                });
-
-              },
-            )
-          ],
-        ));
-  }
+        ),
+      );
 }

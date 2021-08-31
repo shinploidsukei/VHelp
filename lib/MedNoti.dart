2f1a156d63e8cf5a9fb6d2ec2f3817d7a80906b9
shinploidsukeii_ <ployploy1900@hotmail.com>
2021-08-31 17:19:06 +0700

    Update MedNoti for Aor

--- lib/MedNoti.dart
+++ lib/MedNoti.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+import '/dateTime_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:vhelp_test/Content.dart';
 import 'package:vhelp_test/MedConfirmSuccess.dart';
+import 'package:vhelp_test/model/userDate.dart';
 import '/notification_api.dart';
 import 'package:vhelp_test/notification_api.dart';
-
 
 class MedNoti extends StatelessWidget {
   static final String title = 'Local Notifications';
@@ -11,17 +15,17 @@ class MedNoti extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         debugShowCheckedModeBanner: false,
         title: title,
-        theme: ThemeData(primarySwatch: Colors.deepOrange),
-        home: MainPage(),
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        home: MedNotiPage(),
       );
 }
 
-class MainPage extends StatefulWidget {
+class MedNotiPage extends StatefulWidget {
   @override
-  _MainPageState createState() => _MainPageState();
+  _MedNotiPageState createState() => _MedNotiPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MedNotiPageState extends State<MedNotiPage> {
   @override
   void initState() {
     super.initState();
@@ -61,56 +65,163 @@ class _MainPageState extends State<MainPage> {
         ),
       ); */
 
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.amber.shade300,
-        body: Container(
-          padding: EdgeInsets.all(32),
-          child: Column(
-            children: [
-              Spacer(),
-              FlutterLogo(size: 160),
-              const SizedBox(height: 24),
-              Text(
-                'Local Notifications',
-                style: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-           
-              const SizedBox(height: 24),
-              buildButton(
-                title: 'Scheduled Notification',
-                icon: Icons.notifications_active,
-                onClicked: () {
-                  NotificationApi.showScheduledNotification(
-                    title: 'Dinner',
-                    body: 'Today at 6 PM',
-                    payload: 'dinner_6pm',
-                    scheduledDate: DateTime.now().add(Duration(seconds: 12)),
-                  );
+  int index = 0;
+  late Object arguments;
+  TextEditingController controller = TextEditingController();
 
-                  final snackBar = SnackBar(
-                    content: Text(
-                      'Scheduled in 12 Seconds!',
-                      style: TextStyle(fontSize: 24),
-                    ),
-                    backgroundColor: Colors.green,
-                  );
-                  ScaffoldMessenger.of(context)
-                    ..removeCurrentSnackBar()
-                    ..showSnackBar(snackBar);
-                },
-              ),
-              const SizedBox(height: 24),
-              buildButton(
-                title: 'Remove Notifications',
-                icon: Icons.delete_forever,
-                onClicked: () {},
-              ),
-              Spacer(),
-            ],
+  DateTime? medNotiDate;
+  TimeOfDay? medNotiTime;
+  userDate iDate = userDate(medDate: '', medTime: '', notiWord: '');
+
+  String getTextDate() {
+    // ignore: unnecessary_null_comparison
+    if (medNotiDate == null) {
+      return 'Select Date';
+    } else {
+      return DateFormat('dd/MM/yyyy').format(medNotiDate!);
+      // return '${date.month}/${date.day}/${date.year}';
+    }
+  }
+
+  String getTextTime() {
+    // ignore: unnecessary_null_comparison
+    if (medNotiTime == null) {
+      return 'Select Time';
+    } else {
+      final hours = medNotiTime!.hour.toString().padLeft(2, '0');
+      final minutes = medNotiTime!.minute.toString().padLeft(2, '0');
+
+      return '$hours:$minutes';
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.blue.shade300, Colors.blueGrey.shade700])),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Medicine Notification"),
+            backgroundColor: Colors.transparent,
+            elevation: 4.0,
+            leading: Icon(Icons.arrow_back_ios),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 24),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black12),
+                        //padding: MaterialStateProperty.all(EdgeInsets.all(50)),
+                        textStyle:
+                            MaterialStateProperty.all(TextStyle(fontSize: 18))),
+                    child: Text(iDate.medDate = (getTextDate())),
+                    onPressed: () {
+                      pickDate(context);
+                    },
+                  ),
+                ),
+                const SizedBox(height: 24),
+                const SizedBox(height: 24),
+                Text('Time',
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 15),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black12),
+                        //padding: MaterialStateProperty.all(EdgeInsets.all(50)),
+                        textStyle:
+                            MaterialStateProperty.all(TextStyle(fontSize: 18))),
+                    child: Text(iDate.medTime = (getTextTime())),
+                    onPressed: () {
+                      pickTime(context);
+                    },
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Text('Notification Word',
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'playing football',
+                      hintStyle: TextStyle(color: Colors.black26),
+                      contentPadding: EdgeInsets.all(8)),
+                  controller: controller,
+                  onChanged: (String? noti) {
+                    noti = controller.text;
+                    iDate.notiWord = noti;
+                  },
+                ),
+                const SizedBox(height: 24),
+                buildRandomButton(),
+                SizedBox(
+                  height: 20,
+                ),
+
+                Spacer(),
+                // FlutterLogo(size: 160),
+                const SizedBox(height: 24),
+                buildButton(
+                  title: 'Confirm',
+                  icon: Icons.notifications_active,
+                  onClicked: () {
+                    print(medNotiDate.toString()+medNotiTime.toString());
+                   // print(medNotiDate!.setTimeOfDay(time));
+                    NotificationApi.showScheduledNotification(
+                      title: 'VHelp',
+                      body: '${iDate.notiWord}',
+                      payload:
+                          'Date: ${iDate.medDate} \nTime: ${iDate.medTime} \nNotification Word: ${iDate.notiWord}',
+                      scheduledDate: DateTime.now().add(Duration(
+                          days: medNotiDate!.day - DateTime.now().day,
+                          hours: medNotiTime!.hour - DateTime.now().hour,
+                          minutes:
+                              medNotiTime!.minute - DateTime.now().minute)),
+                    );
+
+                    final snackBar = SnackBar(
+              
+                      content: Text(
+                        'Scheduled in ${(medNotiDate!.difference(DateTime.now()).inDays)} day, ${((medNotiDate!.difference(DateTime.now()).inHours) - 24 * medNotiDate!.difference(DateTime.now()).inDays)} hours, ${(medNotiDate!.difference(DateTime.now()).inMinutes).abs() - 60 * (DateTime.now().difference(medNotiDate!).inHours)} minutes',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      backgroundColor: Colors.green,
+                    );
+                    ScaffoldMessenger.of(context)
+                      ..removeCurrentSnackBar()
+                      ..showSnackBar(snackBar);
+                  },
+                ),
+                Spacer(),
+              ],
+              
+            ),
           ),
         ),
       );
@@ -120,23 +231,89 @@ class _MainPageState extends State<MainPage> {
     required IconData icon,
     required VoidCallback onClicked,
   }) =>
-      ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          minimumSize: Size.fromHeight(56),
-          primary: Colors.white,
-          onPrimary: Colors.black,
-          textStyle: TextStyle(fontSize: 20),
-        ),
-        child: Row(
-          children: [
-            Icon(icon, size: 28),
-            const SizedBox(width: 16),
-            Text(title),
-          ],
-        ),
-        onPressed: onClicked,
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              alignment: Alignment.center,
+              minimumSize: Size.fromHeight(56),
+              primary: Colors.green,
+              onPrimary: Colors.white,
+              textStyle: TextStyle(fontSize: 20),
+            ),
+            child: Row(
+              children: [
+                Icon(icon, size: 28),
+                const SizedBox(width: 16),
+                Text(title),
+              ],
+            ),
+            onPressed: onClicked,
+          )
+        ],
       );
+
+  Future pickDate(BuildContext context) async {
+    final newDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(DateTime.now().year - 5),
+      lastDate: DateTime(DateTime.now().year + 5),
+    );
+
+    if (newDate == null) return;
+
+    setState(() => medNotiDate = newDate);
+  }
+
+  Future pickTime(BuildContext context) async {
+    final newTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+
+    if (newTime == null) return;
+
+    setState(() => medNotiTime = newTime);
+  }
+
+  Widget buildRandomButton() {
+    final backgroundColor = MaterialStateColor.resolveWith((states) =>
+        states.contains(MaterialState.pressed)
+            ? Colors.blueGrey
+            : Colors.black38);
+
+    return TextButton.icon(
+      style: ButtonStyle(backgroundColor: backgroundColor),
+      label: Text('Random Words'),
+      icon: Icon(Icons.autorenew, color: Colors.white),
+      onPressed: () {
+        final rWords = getRandomItem();
+        controller.text = rWords;
+        iDate.notiWord = rWords;
+      },
+    );
+  }
+
+  String getRandomItem() {
+    List<String> list = [
+      'กินข้าว',
+      'ดูหนัง',
+      'เตะบอล',
+      'ไปซื้อชานมไข่มุก',
+      'สั่งเซเว่น',
+      'โทรหาแม่ด้วย',
+      'อย่าลืมซักผ้า',
+      'กินชาบู'
+    ];
+    final random = new Random();
+    String item = list[random.nextInt(list.length)];
+    return item;
+  }
 }
+
 
 /*
 
